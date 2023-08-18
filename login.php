dd4b753c4a5a6425ea6bb3006fb3da2b2cc0e59c
nikolasalfa18 <142470571+nikolasalfa18@users.noreply.github.com>
2023-08-18 14:39:12 +0700

    Update and rename login.html to login.php

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php 
  
-include 'config.html';
+include 'config.php';
  
 error_reporting(0);
  
 session_start();
  
 if (isset($_SESSION['username'])) {
-    header("Location: berhasil_login.html");
+    header("Location: berhasil_login.php");
 }
  
 if (isset($_POST['submit'])) {
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
-        header("Location: berhasil_login.html");
+        header("Location: berhasil_login.php");
     } else {
         echo "<script>alert('user atau password Anda salah. Silahkan coba lagi!')</script>";
     }
